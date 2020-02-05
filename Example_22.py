6a3e4b6dcfde79d74b7540171f5e2ab2031a7c55
Adrian Santos <martinezsantosadrian@gmail.com>
2020-02-05 05:43:14 -0500

    Learn python3 the hard way examples.

---
+++ Example_22.py
@@ -0,0 +1,25 @@
+"""
+There worn't be any code in this exercise or the next one, so there's no What You Should See or Study Drills either.
+In fact, this exercise is like one giant Study drills. I'm going to have you do a review of what you have learned
+so far.
+
+First, go back through every exercise you have done so far and write down every word and symbol (another name for
+"character" that you have used.
+Make sure your list of symbols is complete,
+
+Next to each word or symbol, write its name and what it does. If you can't find a name for a symbol in this book,
+then look for it online. IOf you do not know what a word or symbol does, then read about it again and try using
+it in some code.
+You may run into a few things you can't find out or know, so just keep those on the list and be ready to look them
+when you find them.
+
+Once you have your list, spend a few days rewriting the list and doble-checking that it's correct. This may get boring,
+but push through and really neat it down.
+
+Once you have memorized the list and what they do, then step it up by writing out tables of symbols, their names, and
+that they do from memory. If you hit some you can't recall from memory, go back and memorize them again.
+
+WARNING!
+
+The most important thing when doing this exercise is: "There is no failure, only trying."
+"""
---
+++ Example_5.py
@@ -0,0 +1,14 @@
+my_name = "John Doe"
+my_age = 37  # not a lie
+my_height = 180  # centimeters
+my_weight = 80  # kilos
+my_eyes = 'brown'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} centimeters tall.")
+print(f"He's {my_weight} kilos heavy.")
+print(f"Actually that's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth}.")
